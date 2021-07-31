847b44dd04b79c792fb40670ec5d1b711d1dcd35
Roshan Adhikari <nahsorad@gmail.com>
2021-07-31 13:12:30 +0545

    Return JWT on register and login

--- src/main/java/com/roshan/expensetrackerapi/resources/UserResource.java
+++ src/main/java/com/roshan/expensetrackerapi/resources/UserResource.java
@@ -1,7 +1,10 @@
 package com.roshan.expensetrackerapi.resources;
 
+import com.roshan.expensetrackerapi.Constants;
 import com.roshan.expensetrackerapi.domain.User;
 import com.roshan.expensetrackerapi.services.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,9 +33,7 @@ public class UserResource {
 
 		User user = userService.registerUser(firstName, lastName, email,
 				password);
-		Map<String, String> map = new HashMap<>();
-		map.put("message", "registered successfully");
-		return new ResponseEntity<>(map, HttpStatus.OK);
+		return new ResponseEntity<>(generateJWT(user), HttpStatus.OK);
 	}
 
 	@PostMapping("/login")
@@ -39,8 +41,23 @@ public class UserResource {
 		String email = (String) userMap.get("email");
 		String password = (String) userMap.get("password");
 		User user = userService.validateUser(email, password);
-		Map<String, String> map = new HashMap<>();
-		map.put("message", "logged in successfuly");
-		return new ResponseEntity<>(map, HttpStatus.OK);
+		return new ResponseEntity<>(generateJWT(user), HttpStatus.OK);
 	}
+
+	private Map<String, String> generateJWT(User user) {
+		long timeStamp = System.currentTimeMillis();
+		String token = Jwts.builder()
+				.signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
+				.setIssuedAt(new Date(timeStamp))
+				.setExpiration(new Date(timeStamp + Constants.TOKEN_VALIDITY))
+				.claim("userId", user.getUserId())
+				.claim("email", user.getEmail())
+				.claim("firstName", user.getFirstName())
+				.claim("lastName", user.getLastName())
+				.compact();
+		Map<String, String> map = new HashMap<>();
+		map.put("token", token);
+		return map;
+	}
+
 }
